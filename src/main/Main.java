35d2eed3b70c6c6aa7eb34e5f0a45f5cd21eaf61
joctypo <oscargiraldop1999@gmail.com>
2021-04-06 22:18:00 -0500

    segundo oscar

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,47 @@
 package main;
 
 import java.util.LinkedList;
-
+import processing.core.PImage;
 import processing.core.PApplet;
 
-public class Main  extends PApplet{
+public class Main extends PApplet{
 
 	private TCPConnection1 conectado1;
 	private TCPConnection2 conectado2;
 	private int pantalla;
 	private LinkedList <Drags> dragas;
+	PImage inicio01;
+	PImage inicio02;
+	PImage instrucciones;
+	PImage imgMapa;
+	
+	private int posX = 70, posY=140;
+	
+	
+	//Matriz mapa 20*34
+	private int mapa [][] = {
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//1
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//2
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//3
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//4
+			{0,0 ,1,1,1,1,1,0,1,1,1, 0,0,0,0,0,0,0,0,0,0,0,0 ,1,1,1,1,1,0,1,1,1 ,0,0},//5
+			{0,0 ,1,0,1,0,1,0,1,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,1,0,1,0,1,0,1,0,1 ,0,0},//6
+			{0,0 ,1,0,1,0,1,0,1,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,1,0,1,0,1,0,1,0,1 ,0,0},//7
+			{0,0 ,1,0,1,0,1,0,1,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,1,0,1,0,1,0,1,0,1 ,0,0},//8
+			{0,0 ,1,0,1,0,1,0,1,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,1,0,1,0,1,0,1,0,1 ,0,0},//9
+			{0,0 ,0,0,1,0,1,0,1,0,0 ,0,0,0,0,0,0,0,0,0,0,0,0 ,0,0,1,0,1,0,1,0,0 ,0,0},//10
+			{0,0 ,1,0,1,0,1,0,1,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,1,0,1,0,1,0,1,0,1 ,0,0},//11
+			{0,0 ,1,0,1,0,1,0,1,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,1,0,1,0,1,0,1,0,1 ,0,0},//12
+			{0,0 ,1,1,1,0,1,1,1,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,1,1,1,0,1,1,1,0,1 ,0,0},//13
+			{0,0 ,0,0,0,0,1,0,0,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,0,0,0,0,1,0,0,0,1 ,0,0},//14
+			{0,0 ,1,1,1,1,1,1,1,1,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,1,1,1,1,1,1,1,1,1 ,0,0},//15
+			{0,0 ,1,0,1,0,1,0,0,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,1,0,1,0,1,0,0,0,1 ,0,0},//16
+			{0,0 ,0,0,1,0,1,0,1,0,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,0,0,1,0,1,0,1,0,1 ,0,0},//17
+			{0,0 ,4,1,1,0,1,0,1,1,1 ,0,0,0,0,0,0,0,0,0,0,0,0 ,4,1,1,0,1,0,1,1,1 ,0,0},//18
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//19
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},//20
+			
+	};
 	
 	
 	public static void main(String[] args) {
@@ -40,8 +72,14 @@ public class Main  extends PApplet{
     	conectado2.setMain(this);
     	conectado2.start();
     	
+    	//Espacio para cargar las imagenes
+    	inicio01 = loadImage("imagenes/pantalla1.png");
+    	inicio02 = loadImage("imagenes/pantalla1-2.png");
+    	instrucciones = loadImage("imagenes/instrucciones.png");
+    	imgMapa = loadImage("imagenes/mapa.png");
+
     	//Mientras inicia en 3 para poder empezar en el juego, toca cambiarlo a 1
-    	pantalla=3;
+    	pantalla=1;
     	
     
 
@@ -54,7 +92,10 @@ public class Main  extends PApplet{
     	case 1:
     		
     		//Pantalla inicio, toca hacer las zonas sesibles
-    		
+    		image(inicio01,0,0);
+    		/*fill(0);
+			textSize(10);
+			text("X: " + mouseX + " Y " + mouseY, mouseX, mouseY);*/
     		
     		break;
     		
@@ -62,8 +103,10 @@ public class Main  extends PApplet{
     	case 2:
     		
     		// Pantalla de instrucciones 
-    		
-    		
+    		image(instrucciones,0,0);
+    		/*fill(0);
+			textSize(10);
+			text("X: " + mouseX + " Y " + mouseY, mouseX, mouseY);*/
     		break;
     		
     		
@@ -71,7 +114,8 @@ public class Main  extends PApplet{
     	case 3:
     		
     		// Pantalla de juego 
-    		
+    		image(imgMapa,0,0);
+    		ellipse(posX+15,posY+15,15,15);
     		
     		break;
     	
@@ -80,6 +124,29 @@ public class Main  extends PApplet{
     	}
     	
     	
+    	
+    }
+    
+    public void mousePressed() {
+    	switch(pantalla) {
+    	case 1:
+    		if(mouseX > 512 && mouseX < 747 && mouseY > 574 && mouseY < 648) {
+    			pantalla = 2;
+    		}
+    		break;
+    	
+    	case 2:
+    		if(mouseX > 480 && mouseX < 645 && mouseY > 574 && mouseY < 648) {
+    			pantalla = 3;
+    		}
+    		break;
+    		
+    	case 3:
+    		break;
+    	}
+    }
+    
+    public void keyPressed() {
     	
     }
     
@@ -95,7 +162,6 @@ public class Main  extends PApplet{
 			switch(acc.getAccion()) {
 			
 			case "UPSTART":
-				
 				dragas.get(0).arribamuevalo();
 				
 				break;
